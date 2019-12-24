7ef539beb16e3fa389196fb1f23c88d60f2990cc
Hassan El Desouky <hassaneldesouky@icloud.com>
2019-12-25 00:23:59 +0200

    Add new problem

---
+++ 03_ProblemSolvingParadigms/Iterative/One Loop/11001-Necklace.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define el "\n"
+#define F first
+#define S second
+#define PI 3.14159265358979323846  /* pi */
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define RI(i,n) FOR(i,1,n)
+#define REP(i,n) FOR(i,0,(n)-1)
+#define SQR(x) (x)*(x)
+#define all(v)  	((v).begin()),((v).end())
+#define degreesToRadians(angleDegrees) (angleDegrees * PI / 180.0) // Converts degrees to radians.
+#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / PI) // Converts radians to degrees.
+typedef long long ll;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef long double ld;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<pii>		vpii;
+typedef vector<pll>		vpll;
+const double EPS = 1e-9;
+const int N = 1e3+2, M = 3e5+5, OO = 0x3f3f3f3f;
+// int dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; // 4 Direction
+// int dx[] = {1,-1,0,0,1,1,-1,-1}, dy[] = {0,0,1,-1,1,-1,1,-1}; // 8 Direction
+// int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; // Knight Direction
+// int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; // Hexagonal Direction
+int main(){
+	// ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+  int vt, vo;
+  while (1) {
+    scanf("%d %d", &vt, &vo);
+    if (!vt && !vo) break;
+    int p = 0;
+    double Max = 0;
+
+    for (int i = 1; i < vt; i++) {
+      if ((double)vt/i - vo < 0) break;
+      double sum = 0.3*sqrt((double)vt/i - vo)*i;
+      if (fabs(Max - sum) <= 1e-10) {
+        p = 0;
+        break;
+      } else if (sum > Max) {
+        Max = sum;
+        p = i;
+      }
+    }
+    printf("%d\n", p);
+  }
+	return 0;
+}
