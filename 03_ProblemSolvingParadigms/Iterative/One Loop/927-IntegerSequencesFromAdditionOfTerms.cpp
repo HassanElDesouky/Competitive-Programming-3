69422cd74a7340e20d27519b6ce025cefcabaff7
Hassan El Desouky <hassaneldesouky@icloud.com>
2019-12-25 00:04:55 +0200

    Add new problem

---
+++ 03_ProblemSolvingParadigms/Iterative/One Loop/927-IntegerSequencesFromAdditionOfTerms.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define el "\n"
+#define F first
+#define S second
+#define PI 3.14159265358979323846  /* pi */
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define RI(i,n) FOR(i,1,n)
+#define REP(i,n) FOR(i,0,(n)-1)
+#define SQR(x) (x)*(x)
+#define all(v)  	((v).begin()),((v).end())
+#define degreesToRadians(angleDegrees) (angleDegrees * PI / 180.0) // Converts degrees to radians.
+#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / PI) // Converts radians to degrees.
+typedef long long ll;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef long double ld;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<pii>		vpii;
+typedef vector<pll>		vpll;
+const double EPS = 1e-9;
+const int N = 1e3+2, M = 3e5+5, OO = 0x3f3f3f3f;
+// int dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; // 4 Direction
+// int dx[] = {1,-1,0,0,1,1,-1,-1}, dy[] = {0,0,1,-1,1,-1,1,-1}; // 8 Direction
+// int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; // Knight Direction
+// int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; // Hexagonal Direction
+int main(){
+	// ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	int tc; scanf("%d", &tc);
+	while(tc--) {
+		int n; scanf("%d", &n);
+		vector<int> cof(n+1);
+		for(int& i: cof) scanf("%d", &i);
+		int d, k; scanf("%d", &d); scanf("%d", &k);
+
+		int c = 1, t = 0;
+		// while (t < k) t += d*c++;
+		for(int i = 0; i < k;) i+=d*c++;
+		ll ans = 0ll;
+		for(int i = 0; i < cof.size(); ++i) {
+			ans += cof[i] * pow(c-1, i);
+		}
+		printf("%lld\n", ans);
+	}
+	return 0;
+}
