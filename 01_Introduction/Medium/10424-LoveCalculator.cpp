f306a8f2a257d69bf1174dd45f0ba36bf11bebf5
Hassan El Desouky <hassaneldesouky@icloud.com>
2020-03-30 23:49:18 +0200

    Add problems

---
+++ 01_Introduction/Medium/10424-LoveCalculator.cpp
@@ -0,0 +1,88 @@
+
+#include "bits/stdc++.h"
+
+using namespace std;
+#define el "\n"
+#define F first
+#define S second
+#define PI 3.14159265358979323846  /* pi */
+#define INF 9876543210LL
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define RI(i,n) FOR(i,1,n)
+#define REP(i,n) FOR(i,0,(n)-1)
+#define SQR(x) (x)*(x)
+#define all(v)  	((v).begin()),((v).end())
+#define degreesToRadians(angleDegrees) (angleDegrees * PI / 180.0) // Converts degrees to radians.
+#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / PI) // Converts radians to degrees.
+typedef long long ll;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef long double ld;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<pii>		vpii;
+typedef vector<pll>		vpll;
+const double EPS = 1e-9;
+const int N = 1e3+2, M = 3e5+5, OO = 0x3f3f3f3f;
+const string ALPHA = "abcdefghijklmnopqrstuvwxyz";
+// int dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; // 4 Direction
+// int dx[] = {1,-1,0,0,1,1,-1,-1}, dy[] = {0,0,1,-1,1,-1,1,-1}; // 8 Direction
+// int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; // Knight Direction
+// int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; // Hexagonal Direction
+
+int num_of_dig(int n) {
+  int ret = 0;
+  while (n!=0) {
+    ret++;
+    n/=10;
+  }
+  return ret;
+}
+
+int sum_to_digit(int n) {
+  if (num_of_dig(n) == 1) return n;
+  int ret = 0;
+  while (n != 0) { 
+    ret = ret + n % 10; 
+    n = n/10; 
+  } 
+  //cout << ret << el;
+  return sum_to_digit(ret);
+}
+
+int calc_val(string& s1) {
+  int ret = 0;
+  for (int i = 0; i < (int) s1.size(); ++i) {
+      if (s1[i] == ' ') continue;
+      else if ((s1[i] >= 'a' && s1[i] <= 'z') || (s1[i] >= 'A' && s1[i] <= 'Z')) {
+        char x = tolower(s1[i]);
+        string ch = "";
+        ch += x;
+        size_t found = ALPHA.find(ch); 
+        if (found != string::npos) ret += (found+1); 
+      }
+  }
+  return sum_to_digit(ret);
+}
+
+double solve(string s1, string s2) {
+  double x = calc_val(s1), y = calc_val(s2) * 1.0;
+  //cout << x << ' ' << y << el;
+  return y < x ? (double) y / x : (double) x / y;
+}
+
+int main() {
+  // ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  string s1, s2;
+  //cin.ignore();
+  while (getline(cin, s1)) {
+    getline(cin, s2);
+    printf("%0.2lf", (solve(s1, s2)*100));
+    cout << " %\n";
+  }
+  return 0;
+}
+
